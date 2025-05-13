5aa3a682a03d6937508e154f32824067d84c7e7f
AbhinavKumar010 <michaelabhinavthecloud@gmail.com>
2025-05-14 03:50:29 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,9 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: true,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
         format: 'es',
         entryFileNames: 'assets/[name]-[hash].js',
@@ -29,7 +33,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   },
   optimizeDeps: {
