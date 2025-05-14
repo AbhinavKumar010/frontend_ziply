bd01f2c6e29e8faef6304b5694b9348ed3f17f09
AbhinavKumar010 <michaelabhinavthecloud@gmail.com>
2025-05-14 21:59:08 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,14 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
 })
